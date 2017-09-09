9076b2b916bd89d294702d632aa743126d7d50ee
Xristos Diamantis <xristos567@gmail.com>
2017-09-09 11:24:10 +0300

    Update Ask_3.py

--- Ask_3.py
+++ Ask_3.py
@@ -12,8 +12,9 @@ https://www.virustotal.com/en/documentation/public-api/ ώστε να ανεβά
 ποια antivirus βρήκαν το αρχείο να περιέχει κάποιο κακόβουλο λογισμικό.
 '''
 
-import requests
+import requests # includes package requests not part of standard library:  http://docs.python-requests.org/en/master/
 import json
+import time
 
 path=''
 def startFunc():
@@ -34,18 +35,32 @@ params = {'apikey': apikey}
 files = {'file': ('a', file)}
 response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
 json_response = response.json()
+resource=json_response['scan_id']
 
+def reqResponce():
+    global resource
+    params = {'apikey': apikey, 'resource': resource }
+    headers = {
+      "Accept-Encoding": "gzip, deflate",
+      "User-Agent" : "gzip,  My Python requests library example client or username"
+      }
+    response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',params=params, headers=headers)
+    json_response = response.json()
+    if(json_response['response_code']==1):
+        infList=[]
+        for x in json_response['scans']:
+            if json_response['scans'][x]['detected'] == True :
+                infList.append(x)
+    else:
+        time.sleep(3)
+        reqResponce()
 
-params = {'apikey': apikey, 'resource': json_response['scan_id']}
-headers = {
-  "Accept-Encoding": "gzip, deflate",
-  "User-Agent" : "gzip,  My Python requests library example client or username"
-  }
-response = requests.get('https://www.virustotal.com/vtapi/v2/file/report',
-  params=params, headers=headers)
-json_response = response.json()
-if(json_response):
-    print()
+    return infList
 
+infList = reqResponce()
 
-
+if len(infList) == 0:
+    print('Δεν βρέθηκε κακόβουλο λογισμικό')
+else:
+    for name in infList:
+        print(name)
