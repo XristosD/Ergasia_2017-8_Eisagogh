eae3e5f701534082a0e834287d361b8fb87b9b82
Xristos Diamantis <xristos567@gmail.com>
2017-09-12 00:05:19 +0300

    not finished

--- Ask_4.py
+++ Ask_4.py
@@ -14,9 +14,32 @@ Python 3,6
 import requests # includes package requests not part of standard library:  http://docs.python-requests.org/en/master/
 import json
 
-url='https://api.teleport.org/api/cities/?search=San%20Francisco'
-req=requests.get(url)
-reqJs=req.json()
 
+def startRequest():
+    city=input(' Πληκτρολογήστε τα στοιχεία της πόλης: ')
+    city.strip()
+    url='https://api.teleport.org/api/cities/?search='+city
+    req=requests.get(url)
+    reqJs=req.json()
+    if reqJs['count']==0:
+        print('  Η αναζήτηση δεν έδωσε αποτελέσματα. δοκιμάστε ξανά: ')
+        city=startRequest()
+    elif reqJs['count']>1:
+        print('  Η αναζήτση επέστρεψε πολλαπλά αποτελέσματα. Δοκιμάστε κάποια από τις παρακάτω προτάσεις: ')
+        for x in reqJs['_embedded']['city:search-results']:
+            print('    '+x['matching_full_name'])
+        city=startRequest()
+    elif reqJs['count']==1:
+        city=''
+        city=reqJs['_embedded']['city:search-results'][0]['matching_full_name']
+    return city
 
-print(reqJs['count'])
+print('Αναζήτηση Πρώτης Πόλης... ')
+firstC=startRequest()
+print('\nΕπιλέξατε '+firstC)
+print('\n\n')
+print('Αναζήτηση Δεύτερης Πόλης... ')
+secondC=startRequest()
+print('\nΕπιλέξατε '+secondC)
+print(firstC)
+print(secondC)
